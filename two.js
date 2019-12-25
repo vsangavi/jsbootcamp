f0d06ad9753dd1b9e95886b8180bdfbaacdf446e
vsangavi <42089548+vsangavi@users.noreply.github.com>
2019-12-25 14:05:17 +0530

    Add files via upload

---
+++ two.js
@@ -0,0 +1,21 @@
+var f=20;
+var fo=8;
+if(f==34){
+    console.log("hai");
+}
+else if(f==20){
+    console.log("hello");
+}
+else{
+    console.log("something else");
+}
+switch(fo){
+    case 20:
+        console.log("same hello");
+        break;
+    case 34:
+        console.log("same hai");
+        break;
+    default:
+        console.log("something else");
+}
