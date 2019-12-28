f68dd0ca7a4e03df214e1df3df90cdcac27e1031
vsangavi <42089548+vsangavi@users.noreply.github.com>
2019-12-28 08:02:11 +0530

    Add files via upload

---
+++ five.js
@@ -0,0 +1,15 @@
+var i,num;
+num=2;
+for(i=1;i<=5;i++){
+    console.log("hai"+i);
+}
+while(num<=10){
+    console.log("printing while");
+    num++;
+
+}
+num=2;
+do{
+    console.log("do while");
+    num++;
+}while(num<10);
